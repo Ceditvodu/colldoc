e2cf27f0507233be8abc0140ae882cb12497b871
ceditvodu <ceditvodu@gmail.com>
2018-01-21 17:33:34 +0200

    npm link to instal cli

--- index.js
+++ index.js
@@ -5,17 +5,11 @@ const program = require('commander');
 
 program
   .version('0.0.1')
-  .command('hellover <your name> [optional...]')
+  .command('colldoc [optional...]')
   .description('hellover is program that can say hello!')
-  .option('-o, --option','we can still have add\'l options')
   .action(function(req,optional){
-    console.log('\n!!!!!!!!!!!!!!!!!!!!');
-    console.log('well hello %s', req);
-    console.log('!!!!!!!!!!!!!!!!!!!!');
-    if (optional) {
-      optional.forEach(function(opt){
-        console.log("User passed optional arguments: %s!", opt);
-      });
-    }
+    console.log('works');
   });
+
+console.log('hi');
 program.parse(process.argv); // notice that we have to parse in a new statement.
