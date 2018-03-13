dcf3a8297f05c91f91ab35e076ba55f1c7ed9743
ceditvodu <ceditvodu@gmail.com>
2018-03-13 08:10:16 +0200

    order functional wip

--- colldoc.js
+++ colldoc.js
@@ -322,6 +322,41 @@ function syncNewDirectory(path) {
 
 /**
   * @function
+  * @name orderFilesNames
+  * @description Sort files in way that letters will be first.
+  * @param {array} filesNames - list of files names.
+  * @returns {array} - files names with new order.
+  */
+function orderFilesNames(filesNames) {
+  let orderedFilesNames = filesNames.slice().sort( function (a, b) { 
+    let diraction;
+    [].forEach.call(a, (_, index) => {
+      let first = a[index];
+      let second = b[index] || '';
+
+      if ( isNaN(first) !== isNaN(second) ) {
+        if ( isNaN(first) === false ) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+      
+      if ( first < second ) {
+        return 1;
+      } else if ( first > second ) {
+        return -1;
+      }
+
+      return 0;
+
+    });
+  });
+  return orderedFilesNames
+}
+
+/**
+  * @function
   * @name generateMenu
   * @description Html menu generator according list of files and active item.
   * @param {array} filesNames - list of files names.
@@ -329,6 +364,11 @@ function syncNewDirectory(path) {
   * @returns {string} - html menu.
   */
 function generateMenu(filesNames = [], activeItem = '') {
+  
+  let orderedFilesNames = orderFilesNames(filesNames);
+  console.log(orderedFilesNames);
+  
+  // let menu = orderedFilesNames.reduce((a, b) => {
   let menu = filesNames.reduce((a, b) => {
 
     let active = activeItem === b ? 'active' : '';
